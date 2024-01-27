be8cfcee0e4d641104b714f6964e0f2c2a56fac6
leogeier <contact@leonardgeier.com>
2024-01-27 22:13:01 +0100

    cat animations in game

--- game/scenes/cat/cat.gd
+++ game/scenes/cat/cat.gd
@@ -39,9 +39,9 @@ func start_impulse_cooldown():
 	await get_tree().create_timer(.1).timeout
 	impulse_cooldown = false
 
-func _draw():
-	var dir = position.direction_to(last_laser_pointer_position)
-	draw_line(Vector2.ZERO, dir * 100, Color.RED, 3)
+#func _draw():
+	#var dir = position.direction_to(last_laser_pointer_position)
+	#draw_line(Vector2.ZERO, dir * 100, Color.RED, 3)
 
 func _on_laser_pointer_position_changed(pos):
 	last_laser_pointer_position = pos
@@ -91,7 +91,23 @@ func _physics_process(_delta):
 
 #var x = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 func _process(_delta):
-	queue_redraw()
+	if linear_velocity.x < -50:
+		%Icon.flip_h = true
+	elif linear_velocity.x > 50:
+		%Icon.flip_h = false
+	if is_on_ground():
+		if abs(linear_velocity.x) > 10:
+			%Icon.animation = "run"
+		else:
+			if is_laser_close():
+				%Icon.animation = "stand and look"
+			else:
+				%Icon.animation = "idle"
+	else:
+		%Icon.animation = "jump"
+	if not %Icon.is_playing() and not %Icon.animation == "stand and look":
+		%Icon.play()
+	#queue_redraw()
 	gravity_scale = 0.0 if is_on_ground() else 1.0
 	#x.pop_front()
 	#x.append(laser_pointer.velocity)
