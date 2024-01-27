7cd8551c5d087851870316a404f9c0388c3e1272
leogeier <contact@leonardgeier.com>
2024-01-27 23:19:47 +0100

    add paintings

---
+++ game/scenes/obstacles/painting.gd
@@ -0,0 +1,24 @@
+@tool
+extends RigidBody2D
+
+var is_connected = true
+
+func _enter_tree():
+	queue_redraw()
+
+func _physics_process(_delta):
+	if not is_connected: return
+	
+	if abs(rotation) > 0.5:
+		is_connected = false
+		queue_redraw()
+		$"../PinJoint2D".queue_free()
+		
+
+func _draw():
+	if not is_connected: return
+	
+	var start = to_local($"../PinJoint2D".global_position)
+	draw_line(start, $MarkerLeft.position, Color.BLACK, 3, true)
+	draw_line(start, $MarkerRight.position, Color.BLACK, 3, true)
+	
